e6c549da6a6c6a6f35e5dd45474ae3e1e0b3eb52
Yansunsky <1321833118@qq.com>
2026-08-02 19:23:39 +0800

    Adapt to Sodium 0.8.12 & fix crashes (v0.7.0)
    - VoxyConfig: drop Sodium 0.6 OptionStorage implements (caused NoClassDefFoundError on login)
    - Remove SodiumOptionsGUI mixin + VoxyConfigScreenPages (Sodium 0.8 removed old options GUI API)
    - Fix MixinRenderSectionManager.<init> handler signature (SortBehavior param added in 0.8)
    - Fix MixinDefaultChunkRenderer.render handlers (bool param added in 0.8)
    - VoxyNetworkHandler: guard sendToServer with NetworkRegistry.hasChannel (avoid crash on servers without neovoxy)
    - Build against local Sodium 0.8.12 (libs/), bump version to 0.7.0

--- src/main/java/me/cortex/voxy/client/mixin/sodium/MixinDefaultChunkRenderer.java
+++ src/main/java/me/cortex/voxy/client/mixin/sodium/MixinDefaultChunkRenderer.java
@@ -30,7 +30,7 @@ public abstract class MixinDefaultChunkRenderer extends ShaderChunkRenderer {
     @Inject(method = "render", at = @At(value = "HEAD"), cancellable = true)
     private void cancelThingie(ChunkRenderMatrices matrices, CommandList commandList,
             ChunkRenderListIterable renderLists, TerrainRenderPass renderPass, CameraTransform camera,
-            CallbackInfo ci) {
+            boolean var6, CallbackInfo ci) {
         if (VoxyClient.disableSodiumChunkRender()) {
             super.begin(renderPass);
             this.doRender(matrices, renderPass, camera);
@@ -42,7 +42,7 @@ public abstract class MixinDefaultChunkRenderer extends ShaderChunkRenderer {
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/ShaderChunkRenderer;end(Lnet/caffeinemc/mods/sodium/client/render/chunk/terrain/TerrainRenderPass;)V", shift = At.Shift.BEFORE))
     private void injectRender(ChunkRenderMatrices matrices, CommandList commandList,
             ChunkRenderListIterable renderLists, TerrainRenderPass renderPass, CameraTransform camera,
-            CallbackInfo ci) {
+            boolean var6, CallbackInfo ci) {
         this.doRender(matrices, renderPass, camera);
     }
 
